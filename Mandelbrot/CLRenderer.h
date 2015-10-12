21f93cfd2528bcf9ad38a293594f678f2094d95c
Matt Rajca <matt.rajca@me.com>
2015-10-11 23:15:32 -0500

    Factored out CL renderer

---
+++ Mandelbrot/CLRenderer.h
@@ -0,0 +1,12 @@
+//
+//  CLRenderer.h
+//  Mandelbrot
+//
+//  Created by Matt on 10/11/15.
+//  Copyright © 2015 Matt Rajca. All rights reserved.
+//
+
+#import "Renderer.h"
+
+@interface CLRenderer : NSObject <Renderer>
+@end
---
+++ Mandelbrot/CLRenderer.m
@@ -0,0 +1,40 @@
+//
+//  CLRenderer.m
+//  Mandelbrot
+//
+//  Created by Matt on 10/11/15.
+//  Copyright © 2015 Matt Rajca. All rights reserved.
+//
+
+#import "CLRenderer.h"
+
+#import <OpenGL/gl.h>
+#import <OpenCL/OpenCL.h>
+#import "mandelbrot.cl.h"
+
+@implementation CLRenderer
+
+- (void)prepare { }
+
+- (void)renderInContext:(id<RenderContext>)context {
+	dispatch_queue_t queue = gcl_create_dispatch_queue(CL_DEVICE_TYPE_GPU, 0);
+
+	[context allocateTextureWithHandler:^(GLuint texture, void (^token)()) {
+		cl_image image = gcl_gl_create_image_from_texture(GL_TEXTURE_2D, 0, texture);
+
+		dispatch_async(queue, ^{
+			cl_ndrange range = { 2, {0,0}, { IMAGE_SIZE, IMAGE_SIZE }, {0,0} };
+			mandelbrot_kernel(&range, image);
+		});
+
+		dispatch_barrier_sync(queue, ^{
+			dispatch_async(dispatch_get_main_queue(), ^{
+				token();
+			});
+		});
+
+		gcl_release_image(image);
+	}];
+}
+
+@end
