8663c0e79ca9803cbbb821acb94c8ef710fe501d
andre <andre@h1ro.local>
2017-07-30 10:28:22 -0700

    Swift4 - no warnings!

--- Mandelbrot/MetalRenderer.swift
+++ Mandelbrot/MetalRenderer.swift
@@ -18,52 +18,52 @@ class MetalRenderer: NSObject, Renderer {
         let outTexture = device.makeTexture(descriptor: outputTextureDescriptor)
         
         let buffer = commandQueue.makeCommandBuffer()
-        let encoder = buffer.makeComputeCommandEncoder()
-        encoder.setComputePipelineState(computePipelineState)
-        encoder.setTexture(outTexture, at: 0)
+        let encoder = buffer?.makeComputeCommandEncoder()
+        encoder?.setComputePipelineState(computePipelineState)
+        encoder?.setTexture(outTexture, index: 0)
         
         let threadGroupCount = MTLSizeMake(32, 32, 1)
-        let threadGroups = MTLSizeMake(outTexture.width / threadGroupCount.width, outTexture.height / threadGroupCount.height, 1)
+        let threadGroups = MTLSizeMake((outTexture?.width)! / threadGroupCount.width, (outTexture?.height)! / threadGroupCount.height, 1)
         
-        encoder.dispatchThreadgroups(threadGroups, threadsPerThreadgroup: threadGroupCount)
-        encoder.endEncoding()
+        encoder?.dispatchThreadgroups(threadGroups, threadsPerThreadgroup: threadGroupCount)
+        encoder?.endEncoding()
         
-        buffer.commit()
-        buffer.waitUntilCompleted()
+        buffer?.commit()
+        buffer?.waitUntilCompleted()
         
         context.renderedTexture(outTexture)
     }
     
-	private let device: MTLDevice
+    private let device: MTLDevice
 
-	private var commandQueue: MTLCommandQueue!
-	private var outputTextureDescriptor: MTLTextureDescriptor!
-	private var computePipelineState: MTLComputePipelineState?
+    private var commandQueue: MTLCommandQueue!
+    private var outputTextureDescriptor: MTLTextureDescriptor!
+    private var computePipelineState: MTLComputePipelineState?
 
-	@objc var isPrepared = false
+    @objc var isPrepared = false
 
-	init(device: MTLDevice) {
-		self.device = device
+    @objc init(device: MTLDevice) {
+        self.device = device
 
-		super.init()
-	}
+        super.init()
+    }
 
-	func prepare() {
-		guard let library = device.newDefaultLibrary() else {
-			fatalError("Could not get the default library")
-		}
+    func prepare() {
+        guard let library = device.makeDefaultLibrary() else {
+            fatalError("Could not get the default library")
+        }
 
         guard let mandelbrotFunction = library.makeFunction(name: "mandelbrot") else {
-			fatalError("Could not get the mandelbrot function")
-		}
+            fatalError("Could not get the mandelbrot function")
+        }
 
         device.makeComputePipelineState(function: mandelbrotFunction) { state, error in
-			if let state = state {
-				self.computePipelineState = state
-			} else if let error = error {
-				print("Could not create the pipeline state: \(error)")
-			}
-		}
+            if let state = state {
+                self.computePipelineState = state
+            } else if let error = error {
+                print("Could not create the pipeline state: \(error)")
+            }
+        }
 
         commandQueue = device.makeCommandQueue()
 
@@ -72,7 +72,7 @@ class MetalRenderer: NSObject, Renderer {
         outputTextureDescriptor.storageMode = MTLStorageMode.private
         outputTextureDescriptor.usage = [.shaderWrite, .shaderRead];
 
-		isPrepared = true
-	}
+        isPrepared = true
+    }
 
 }
